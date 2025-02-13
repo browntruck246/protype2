8c968f56be404d92c606849d25fac4b0227a50fe
John Smith <browntruck246@gmail.com>
2025-02-13 10:38:56 -0500

    init commit

---
+++ src/main/java/com/sears/controllers/MainController.java
@@ -0,0 +1,100 @@
+package com.sears.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sears.models.Product;
+import com.sears.services.MainServices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        System.out.println("*** sayHello ***" );
+
+        return "Hello, World!";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        System.out.println("*** home ***" );
+        
+        MainServices service = new MainServices();
+        
+        List<Product> products = service.allProducts();
+        
+        model.addAttribute("products", products);
+
+        return "index";
+    }
+    
+    @GetMapping("/add")
+    public String addProductForm(Model model) {
+    	System.out.println("*** addProductForm ***" );
+    	
+        model.addAttribute("product", new Product());
+        return "add-product";
+    }
+    
+    @PostMapping("/add")
+    public String addProduct(@ModelAttribute Product product) {
+    	System.out.println("*** addProduct ***" );
+    	
+    	MainServices service = new MainServices();
+    	
+    	service.saveProduct(product);
+        return "redirect:/home";
+    }
+    
+    @GetMapping("/edit/{id}")
+    public String editProductForm(@PathVariable String id, Model model) {
+    	System.out.println("*** editProductForm ***" );
+    	
+    	MainServices service = new MainServices();
+    	
+        Product product = service.findProduct(id);
+        model.addAttribute("product", product);
+        
+        return "edit-product";
+    }
+    
+    @PostMapping("/edit/{id}")
+    public String editProduct(@PathVariable String id, @ModelAttribute Product product) {
+    	System.out.println("*** editProduct ***" );
+
+    	MainServices service = new MainServices();
+    	
+    	service.saveProduct(product);
+    	
+        return "redirect:/home";
+    }
+    
+    
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable String id) {
+    	System.out.println("*** deleteProduct ***" );
+    	
+    	MainServices service = new MainServices();
+    	
+    	service.deleteProduct(id);
+  
+        return "redirect:/home";
+    }
+    
+    
+    
+
+}
---
+++ src/main/java/com/sears/services/MainServices.java
@@ -0,0 +1,142 @@
+package com.sears.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sears.models.Product;
+
+public class MainServices {
+	
+	 // Database credentials
+    String url = "jdbc:mysql://localhost:3306/protype";
+    String username = "root";
+    String password = "root";
+    
+	public MainServices() {
+		super();
+	}
+
+	public List<Product> allProducts() {
+		
+		List<Product> products = new ArrayList<>();
+		
+        // Establish a connection
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+
+            // Create a statement object
+            Statement statement = connection.createStatement();
+
+            // Execute a SELECT query
+            String query = "SELECT id, name, description, price FROM protype.product;";
+            ResultSet resultSet = statement.executeQuery(query);
+            
+            
+            // Process the result set
+            while (resultSet.next()) {
+            	
+            	Product product = new Product();
+            	
+            	product.setId(resultSet.getInt("id"));
+            	product.setName(resultSet.getString("name"));
+            	product.setDescription(resultSet.getString("description"));
+            	product.setPrice(resultSet.getString("price"));
+            	
+            	products.add(product) ;
+            	
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Cannot connect to the database!");
+            e.printStackTrace();
+        }
+		return products;
+   }
+	
+	public void saveProduct(Product product) {
+		
+		
+        // Establish a connection
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+
+            // Create a statement object
+            Statement statement = connection.createStatement();
+
+         // Using executeUpdate for INSERT, UPDATE, or DELETE statements
+            String insertSQL  = "INSERT INTO protype.product(name, description, price) VALUES ( '" + product.getName() + "', '" + product.getDescription() + "', '" + product.getPrice() +"');";
+            int rowsAffected = statement.executeUpdate(insertSQL);
+            
+            System.out.println("Rows affected: " + rowsAffected);
+            
+        } catch (SQLException e) {
+            System.err.println("Cannot connect to the database!");
+            e.printStackTrace();
+        }
+   }
+	
+	public Product findProduct(String id) {
+		
+		Product product = new Product();
+        // Establish a connection
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+
+            // Create a statement object
+            Statement statement = connection.createStatement();
+
+            // Execute a SELECT query
+            String query = "SELECT id, name, description, price FROM protype.product where id = " + id + ";";
+            ResultSet resultSet = statement.executeQuery(query);
+            
+            // Process the result set
+            while (resultSet.next()) {
+            	
+            	product.setId(resultSet.getInt("id"));
+            	product.setName(resultSet.getString("name"));
+            	product.setDescription(resultSet.getString("description"));
+            	product.setPrice(resultSet.getString("price"));
+            	
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Cannot connect to the database!");
+            e.printStackTrace();
+        }
+		return product;
+   }
+	
+	
+	public void deleteProduct(String id) {
+		
+		
+        // Establish a connection
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+
+            // Create a statement object
+            Statement statement = connection.createStatement();
+
+            // Using executeUpdate for INSERT, UPDATE, or DELETE statements
+            String insertSQL  = "DELETE FROM protype.product where id = " + id + ";";
+            int rowsAffected = statement.executeUpdate(insertSQL);
+            
+            System.out.println("Rows affected: " + rowsAffected);
+            
+        } catch (SQLException e) {
+            System.err.println("Cannot connect to the database!");
+            e.printStackTrace();
+        }
+   }
+	
+	
+	
+
+
+}
